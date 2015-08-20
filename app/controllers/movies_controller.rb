4498767d9142a606f170e7ab9a1fb777bc28f8cd
keyannam <keyannamayfield@gmail.com>
2015-08-20 14:51:42 -0500

    Added clickable image to home page, added sort to columns

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def home
   end
-  
+
   def index
-    @movies = Movie.all
+    @movies = Movie.order(sort_column + " " + sort_direction)
   end
 
   def show
@@ -22,7 +23,7 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
 
       if @movie.save
-        redirect_to @movie, notice: 'Movie was successfully created.'
+        redirect_to movies_path, notice: 'Movie was successfully created.'
       else
         render :new
       end
@@ -41,12 +42,22 @@ class MoviesController < ApplicationController
       redirect_to movies_path, notice: 'Movie was successfully destroyed.'
   end
 
-  private
-    def set_movie
-      @movie = Movie.find(params[:id])
-    end
 
-    def movie_params
-      params.require(:movie).permit(:title, :format, :length, :release_year, :rating)
-    end
+  private
+
+  def sort_column
+    Movie.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :format, :length, :release_year, :rating)
+  end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,8 @@
 module MoviesHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    link_to title, movies_path(:sort => column, :direction => direction), {:class => css_class}
+  end
 end
